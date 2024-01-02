9a6b32444e5010e1e6b4cf2d486595567c9f39c2
PimSc <pimschuitema@gmail.com>
2024-01-02 10:45:07 +0100

    bezig geweest met navigatie balk (voor telefoon gebruik) nog niet af

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,35 +1,74 @@
-import './Navigation.css'
-import {NavLink} from "react-router-dom";
-
-
-
-// Navigation component is imported directly into App.jsx
-
-
+import './Navigation.css';
+import { NavLink } from 'react-router-dom';
+// import menuIcon from "./../../assets/burgerMenuIcon.png"
 
 
 function Navigation() {
 
+
+
     return (
+        <div className="nav-container-main">
 
-        <div className="nav-container-main" >
-            <nav>
-        <ul>
-            <li className="mainLink" ><NavLink to="/" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Home</NavLink></li>
-            <li className="mainLink"><NavLink to="/OnsVerhaal" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons verhaal</NavLink></li>
-            {/*<li><NavLink to="/Blogs" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Blogs</NavLink></li>*/}
-            <li className="mainLink"><NavLink to="/Excursies" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink></li>
-            <li className="mainLink"><NavLink to="/Account" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Account</NavLink></li>
-            <li className="mainLink"><NavLink to="/Contact" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink></li>
-            <form action="/search" method="get">
-                <input type="text" name="query" placeholder="Zoek..."/>
-            </form>
-        </ul>
+
+            {/*Start main navigation bar*/}
+            <nav className="mainNav">
+                        <ul>
+                            <li><NavLink to="/" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Homemain</NavLink></li>
+                            <li><NavLink to="/OnsVerhaal" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons verhaal</NavLink></li>
+                            {/*<li><NavLink to="/Blogs" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Blogs</NavLink></li>*/}
+                            <li><NavLink to="/Excursies" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink></li>
+                            <li><NavLink to="/Account" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Account</NavLink></li>
+                            <li><NavLink to="/Contact" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink></li>
+                            <form action="/search" method="get">
+                                <input type="text" name="query" placeholder="Zoek..."/>
+                            </form>
+                        </ul>
+                    </nav>
+            {/*End main navigation bar*/}
+
+
+
+
+
+            {/*Start phone navigation bar*/}
+            <nav className="phoneNav">
+                <ul>
+                    <li><NavLink to="/" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Hometel</NavLink></li>
+                    <li><NavLink to="/OnsVerhaal" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons verhaal</NavLink></li>
+                    {/*<li><NavLink to="/Blogs" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Blogs</NavLink></li>*/}
+                    <li><NavLink to="/Excursies" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink></li>
+                    <li><NavLink to="/Account" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Account</NavLink></li>
+                    <li><NavLink to="/Contact" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink></li>
+
+                    <form action="/search" method="get">
+                        <input type="text" name="query" placeholder="Zoek..."/>
+                    </form>
+                </ul>
             </nav>
+            {/*End phone navigation bar*/}
 
+
+
+            {/*/!*Start phone navigation bar*!/*/}
+            {/*<nav className="phoneNav">*/}
+            {/*    <ul>*/}
+            {/*        <li><NavLink to="/" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Home</NavLink></li>*/}
+            {/*        <li><NavLink to="/OnsVerhaal" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons verhaal</NavLink></li>*/}
+            {/*        /!*<li><NavLink to="/Blogs" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Blogs</NavLink></li>*!/*/}
+            {/*        <li><NavLink to="/Excursies" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink></li>*/}
+            {/*        <li><NavLink to="/Account" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Account</NavLink></li>*/}
+            {/*        <li><NavLink to="/Contact" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink></li>*/}
+
+            {/*        <form action="/search" method="get">*/}
+            {/*            <input type="text" name="query" placeholder="Zoek..."/>*/}
+            {/*        </form>*/}
+            {/*    </ul>*/}
+            {/*</nav>*/}
+            {/*/!*End phone navigation bar*!/*/}
 
         </div>
-    )
+    );
 }
 
-export default Navigation
+export default Navigation;
