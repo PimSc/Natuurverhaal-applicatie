c754fe19dd3b59b8243c01f28db99515cb50adef
PimSc <pimschuitema@gmail.com>
2024-01-04 15:23:34 +0100

    afbeeldingen aangepast

--- src/pages/excursies/Excursies.jsx
+++ src/pages/excursies/Excursies.jsx
@@ -1,6 +1,7 @@
 import './Excursies.css'
 import ExcursionBanner from "../../components/excursionBanner/ExcursionBanner.jsx";
-import foto from "../../assets/ExcursionSideImage.jpg"
+import ExcursionSideImageMain from "../../assets/ExcursionSideImageMain.jpg"
+import ExcursionSideImagePhone from "../../assets/ExcursionSideImagePhone.jpg"
 
 
 function Excursies() {
@@ -15,18 +16,20 @@ function Excursies() {
 
                     <p>excursies pagina</p>
 
-                    <div className="excursionSideImage">
-                        <img src={foto} alt="Een aantal mensen lopen door een natuurlijke vallei van watervallen en naaldbos, met een achtergrond waarin bergen opgaan in de mist en een voorgrond met prachtig gekleurde bloemen in roze, geel en oranje" />
+                    <div className="excursionSideImageMain">
+                        <img src={ExcursionSideImageMain} alt="Een aantal mensen lopen door een natuurlijke vallei van watervallen en naaldbos, met een achtergrond waarin bergen opgaan in de mist en een voorgrond met prachtig gekleurde bloemen in roze, geel en oranje" />
                     </div>
-
-
-
-
-
-
+                    <div className="excursionSideImagePhone">
+                        <img src={ExcursionSideImagePhone} alt="Een aantal mensen lopen door een natuurlijke vallei van watervallen en naaldbos, met een achtergrond waarin bergen opgaan in de mist en een voorgrond met prachtig gekleurde bloemen in roze, geel en oranje" />
+                    </div>
 
                 </div>
             </div>
+
+
+
+
+
         </>
     )
 }
