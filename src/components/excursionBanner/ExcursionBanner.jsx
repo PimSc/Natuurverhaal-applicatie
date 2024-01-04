c173211306b774cf22efeb523ca9ca763ad3311b
PimSc <pimschuitema@gmail.com>
2024-01-04 13:41:01 +0100

    lekker bezig, commit tijdens het inrichten van de excursies pagina

---
+++ src/components/excursionBanner/ExcursionBanner.jsx
@@ -0,0 +1,16 @@
+import './ExcursionBanner.css';
+
+function ExcursionBanner() {
+
+return (
+<>
+<div className="excursionBanner">
+    <img src="src/assets/excursionBanner1.jpg" alt="A group of people is walking along a river in nature; it's early in the morning, and the sun is rising slowly." />
+</div>
+</>
+);
+}
+
+export default ExcursionBanner;
+
+
--- src/pages/excursies/Excursies.jsx
+++ src/pages/excursies/Excursies.jsx
@@ -1,4 +1,6 @@
 import './Excursies.css'
+import ExcursionBanner from "../../components/excursionBanner/ExcursionBanner.jsx";
+
 
 function Excursies() {
 
@@ -6,12 +8,24 @@ function Excursies() {
     return (
         <>
 
+            <ExcursionBanner />
+            <div className="outer-content-container">
+                <div className="inner-content-container">
+
+                    <p>excursies pagina</p>
+
+                    <div className="excursionSideImage">
+                        <img src="src/assets/ExcursionSideImage.jpg" alt="Description of the image" />
+                    </div>
 
 
-excursies pagina
 
 
 
+
+
+                </div>
+            </div>
         </>
     )
 }
