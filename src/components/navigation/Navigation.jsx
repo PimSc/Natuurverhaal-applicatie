0462cf979f5ec7c03bec38ffaf62f8b80f33bbc3
PimSc <pimschuitema@gmail.com>
2024-01-19 23:10:00 +0100

    header en telefoon zoekbalk aangepast aan media querie

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -7,18 +7,26 @@ function Navigation() {
 
     return (
         <>
-        <div className="navContainerStickyTop">  {/*Sticky container*/}
+            <div className="navContainerStickyTop">  {/*Sticky container*/}
 
-            {/*Start main navigation bar (computer screen)*/}
-            <nav className="mainNav">
-                        <ul>
-                            <li><NavLink to="/" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Home</NavLink></li>
-                            <li><NavLink to="/OnsVerhaal" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons verhaal</NavLink></li>
-                            <li><NavLink to="/Excursies" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink></li>
-                            <li><NavLink to="/Prikbord" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Prikbord</NavLink></li>
+                {/*Start main navigation bar (computer screen)*/}
+                <nav className="mainNav">
+                    <ul>
+                        <li><NavLink to="/"
+                                     className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Home</NavLink>
+                        </li>
+                        <li><NavLink to="/OnsVerhaal"
+                                     className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons
+                            verhaal</NavLink></li>
+                        <li><NavLink to="/Excursies"
+                                     className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink>
+                        </li>
+                        <li><NavLink to="/Prikbord"
+                                     className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Prikbord</NavLink>
+                        </li>
 
-                            <li>
-                                <div className="dropdown">
+                        <li>
+                            <div className="dropdown">
                                 <button className="dropbtn">Account</button>
                                 <div className="dropdown-content">
                                     <li><Link to="/login">Login</Link></li>
@@ -27,40 +35,59 @@ function Navigation() {
                                     <li><Link to="/">Uitloggen</Link></li>
                                 </div>
                             </div>
-                            </li>
+                        </li>
 
-                            <li><NavLink to="/Contact" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink></li>
-                            <SearchBar />  {/*Searchbar component*/}
-                        </ul>
-                    </nav>
-            {/*End main navigation bar*/}
-
-            {/*Start telephone navigation bar (phone screen)*/}
-
-            <div className="hamburgerEffect">
-                <label className="hamburger-menu">
-                <input type="checkbox" />
-                </label>
-
-                <aside className="sidebar">
-                <nav>
-                    <li><NavLink to="/" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Home</NavLink></li>
-                    <li><NavLink to="/OnsVerhaal" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons verhaal</NavLink></li>
-                    <li><NavLink to="/Excursies" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink></li>
-                    <li><NavLink to="/Prikbord" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Prikbord</NavLink></li>
-                    <li><NavLink to="/login" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Login</NavLink></li>
-                    <li><NavLink to="/mijnBlogs" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Blogs</NavLink></li>
-                    <li><NavLink to="/Profiel" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Profiel</NavLink></li>
-                    <li><NavLink to="/Contact" className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink></li>
-                    <NavLink to="/" className={({ isActive }) => isActive === true ? 'active-link' : 'default-link'}>Uitloggen</NavLink>
+                        <li><NavLink to="/Contact"
+                                     className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink>
+                        </li>
+                        <SearchBar/> {/*Searchbar component*/}
+                    </ul>
                 </nav>
-                </aside>
+                {/*End main navigation bar*/}
 
-            <div className="phoneSearchBar">
-                <SearchBar />  {/*Searchbar component*/}
+                {/*Start telephone navigation bar (phone screen)*/}
+
+                <div className="hamburgerEffect">
+                    <label className="hamburger-menu">
+                        <input type="checkbox"/>
+                    </label>
+
+                    <aside className="sidebar">
+                        <nav>
+                            <li><NavLink to="/"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Home</NavLink>
+                            </li>
+                            <li><NavLink to="/OnsVerhaal"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Ons
+                                verhaal</NavLink></li>
+                            <li><NavLink to="/Excursies"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Excursies</NavLink>
+                            </li>
+                            <li><NavLink to="/Prikbord"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Prikbord</NavLink>
+                            </li>
+                            <li><NavLink to="/login"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Login</NavLink>
+                            </li>
+                            <li><NavLink to="/mijnBlogs"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Blogs</NavLink>
+                            </li>
+                            <li><NavLink to="/Profiel"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Profiel</NavLink>
+                            </li>
+                            <li><NavLink to="/Contact"
+                                         className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Contact</NavLink>
+                            </li>
+                            <NavLink to="/"
+                                     className={({isActive}) => isActive === true ? 'active-link' : 'default-link'}>Uitloggen</NavLink>
+                        </nav>
+                    </aside>
+
+                    <div className="phoneSearchBar">
+                        <SearchBar/> {/*Searchbar component*/}
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
         </>
     );
 }
