79084e06356a580fcd762a175c1f5cec87105947
PimSc <pimschuitema@gmail.com>
2024-01-20 10:35:04 +0100

    De zoekbalk is nu alleen nog zichtbaar op home, excursie, prikbord pagina

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,16 +1,25 @@
+import { useLocation } from 'react-router-dom';
 import './SearchBar.css';
 
 function SearchBar() {
 
-return (
-<>
+    // Haal de huidige locatie op met behulp van useLocation() van react-router-dom.
+    const location = useLocation();
 
-    <form action="/search" method="get">
-        <input className="SearchBar" type="text" name="query" placeholder="Zoek..."/>
-    </form>
+    // bepaalt op welke pagina`s het zoekbalk component zichtbaar is
+    const allowedPages = ['/',"/Excursies","/Prikbord"];
+    const showSearchBar = allowedPages.includes(location.pathname);
 
-</>
-);
+    // Als de huidige locatie niet in de allowedPages array staat, return null.
+    if (!showSearchBar) {
+        return null;
+    }
+
+    return (
+        <form>
+            <input className="SearchBar" type="text" name="query" placeholder="Zoek..." />
+        </form>
+    );
 }
 
 export default SearchBar;
