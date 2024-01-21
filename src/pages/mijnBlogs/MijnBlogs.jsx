826d0dce46a41784bb43280b6458d0ddd8125768
PimSc <pimschuitema@gmail.com>
2024-01-21 16:27:03 +0100

    vergeten een nieuwe brange aan te maken, ga ik nu doen

--- src/pages/mijnBlogs/MijnBlogs.jsx
+++ src/pages/mijnBlogs/MijnBlogs.jsx
@@ -1,20 +1,26 @@
 import './MijnBlogs.css';
- 
+import BlogOverview from "../../components/blogOverview/BlogOverview.jsx";
+// import BlogCounter from "../../components/blogCounter/Blogcounter.jsx";
+
 function MijnBlogs() {
 
-return (
-<>
-    <div className="outer-content-container">
-        <div className="inner-content-container-column">
-            <p>Blogs overzicht</p>
-            <p>Nieuw blog schijven</p>
-            <p>Blogs concepten</p>
-            <p>Blogs aanpassen</p>
-            <p>Blogs verwijderen</p>
-        </div>
-    </div>
-</>
-);
+    return (
+        <>
+            <div className="outer-content-container">
+                <div className="inner-content-container-column">
+
+                    <button className="SimpleButtons" id="WriteBlogButton" type="submit" onClick="">
+                        Blog schrijven
+                    </button>
+                    <br/>
+                    <h1 id="myBlogsTitleMargin">Mijn blogs</h1>
+                    <BlogOverview />
+
+
+                </div>
+            </div>
+        </>
+    );
 }
- 
+
 export default MijnBlogs;
