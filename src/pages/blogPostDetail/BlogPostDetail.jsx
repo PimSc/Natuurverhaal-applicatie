9cf063c0f806afd8daed4451e3c5525540e1191a
PimSc <pimschuitema@gmail.com>
2024-02-19 10:38:05 +0100

    backend blog ophalen op de blogDetail pagina werkt ook

--- src/pages/blogPostDetail/BlogPostDetail.jsx
+++ src/pages/blogPostDetail/BlogPostDetail.jsx
@@ -1,35 +1,40 @@
 import './BlogPostDetail.css';
 import { Link, useParams } from 'react-router-dom';
-import posts from '../../constants/BlogPosts.json';
 import formatDateString from '../../helpers/formatDateString.jsx';
+import useBlog from "../../Hooks/useBlog.jsx";
+import image from './../../../public/assets/blogPostImages/wolfPhoto.jpg';
 
-
-// Op deze pagina wordt 1 specifieke blogpost getoond wanneer deze op de homepage wordt aangeklikt.
 
 function BlogPostDetail() {
-    const { id } = useParams();
 
-    const { image, caption, title, subtitle, author, created, content,} = posts.find((post) => {
-        return post.id.toString() === id;
-    });
+
+        const { id } = useParams(); // Haal het ID uit de URL-parameters
+        const { blogPosts } = useBlog();
+        const post = blogPosts.find(post => post.id.toString() === id); // Zoek de blogpost met het overeenkomende ID
+
+        if (!post) {
+            return <div>Blogpost niet gevonden</div>; // Toon een foutmelding als de blogpost niet wordt gevonden
+        }
+
+
 
     return (
         <>
             <section className="outer-content-container-column">
-                <img src={image} alt={caption}/>
+                <img src={image} alt="-"/>
 
                 <div className="inner-content-container">
 
 
                     <div className="textCenter">
-                    <h1>{title}</h1>
+                    <h1>{post.title}</h1>
                         <br/>
-                    <h3>{subtitle}</h3>
+                    <h3>{post.subtitle}</h3>
                         <div className="textStart">
                         </div>
-                    <p>Geschreven door <em>{author}</em> op {formatDateString(created)}</p>
+                    {/*<p>Geschreven door <em>{post.author}</em> op {formatDateString(post.created)}</p>*/}
                     <br/>
-                    <p className="textStart">{content}</p>
+                    <p className="textStart">{post.content}</p>
 
                     <Link to="/" className="back-link">
                         <br/>
